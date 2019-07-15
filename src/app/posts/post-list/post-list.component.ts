6f01f3ada1d6b68722f1d5c1fb16a03a6f3183da
Brian Workman <brianworkman43055@gmail.com>
2019-07-15 00:45:27 -0400

    Adding post components

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  posts = [
+    {title: 'First Post', content: 'this is the first posts content'},
+    {title: 'Second Post', content: 'this is the second posts content'},
+    {title: 'Third Post', content: 'this is the third posts content'},
+  ];
+
+
+}
